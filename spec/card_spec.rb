88b7fa5b3bfd9d91120c380662c61fd8e7742658
Rose Lu <roseyylu@gmail.com>
2015-04-28 16:03:12 +1200

    amending test to pass and use proper rspec syntax

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -4,8 +4,8 @@ describe Card do
   describe "can make new card" do
     it "can make regular card" do
       card = Card.new "4H"
-      expect card.value == "4"
-      expect card.suit == "Hearts"
+      expect(card.value).to eq("4")
+      expect(card.suit).to eq("H")
     end
   end
 
